33527e1cde75fb20431fd469ace20b7bc4d35d64
HUSTToanLH <laihuutoan@gmail.com>
2016-03-24 18:52:04 +0700

    Namespace admin and delete user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  include UsersHelper
 
   def logged_in_user
     unless logged_in?
       flash[:danger] = t "views.login.notice"
       redirect_to login_url
+    end
+  end
+
+  def verify_admin
+    unless current_user.admin?
+      flash[:danger] = t "admin.flash.permission"
+      redirect_to root_url
     end
   end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,16 @@
 class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
+  def index
+    @user = User.find params[:user_id]
+    if Settings.relationships.include? params[:relationship]
+      @title = params[:relationship]
+      @users = @user.send(params[:relationship]).paginate page: params[:page]
+    else
+      redirect_to request.referrer
+    end
+  end
+
   def create
     @user = User.find params[:followed_id]
     current_user.follow @user
