1e1a2da20e59a1da000492bd1593607601432ee4
HUSTToanLH <laihuutoan@gmail.com>
2016-03-25 17:08:11 +0700

    Admin home & delete category

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   namespace :admin do
     root "categories#index"
     resources :users
-    resources :categories, except: [:show, :destroy]
+    resources :categories, except: :show
   end
 end
